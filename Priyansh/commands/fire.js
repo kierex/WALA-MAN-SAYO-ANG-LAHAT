1b50b417f24e13befb79376088fe0fe71025dc68
crazyvrax <heroxhonattackz20@gmail.com>
2025-05-21 07:42:50 +0800

    Update fire.js

--- Priyansh/commands/fire.js
+++ Priyansh/commands/fire.js
@@ -1,52 +1,39 @@
 module.exports.config = {
-    name: "Fire",
-    version: "1.0.0",
-    hasPermssion: 2,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-    description: "Fire broke the boxchat",
-    commandCategory: "group",
-    usages: "bold Fire",
-    cooldowns: 10,
-    dependencies: {
-        "fs-extra": "",
-        "axios": ""
-    }
-}
+  name: "roast",
+  version: "1.0.0",
+  hasPermssion: 2,
+  credits: "Vrax",
+  description: "Playfully roasts the tagged user",
+  commandCategory: "fun",
+  usages: "@tag",
+  cooldowns: 10,
+  dependencies: {}
+};
 
-module.exports.run = async function({ api, args, Users, event}) {
- var mention = Object.keys(event.mentions)[0];
-    
- let name =  event.mentions[mention];
-    var arraytag = [];
-        arraytag.push({id: mention});
-    var a = function (a) { api.sendMessage(a, event.threadID); }
-a("T3R9 B99P BOT 3NT3R S9L3 APN3 B99P S3 JUB99N L9D9Y3G9🤞🏻👅👅");
-setTimeout(() => {a({body: " BOT KO G9LI D3N3 W9LO TUMH9RII B9HN KII (+)🙊💋" })}, 3000);
-setTimeout(() => {a({body: " T3RII M99 BHOSD99 F99D KR F3KH DUNG9 S9L33 H9WB99Z T9TT3💋🤣"})}, 5000);
-setTimeout(() => {a({body: " T3RII M99 K3 BHOSD33 M3 MOOT DUNG99 M9RCHOXDD😜💔" })}, 7000);
-setTimeout(() => {a({body: " T3RII M9 KI (+) R9ND K3 B9XH33🤚🏻💋" })}, 9000);
-setTimeout(() => {a({body: " T3RII B9HN KO YHI P979K KR CHOD DUNG99 S9L3😈😍" })}, 12000);
-setTimeout(() => {a({body: " T3RI B9HN KI G9ND P3 LOD9 F3KH KR M9RUNG9 S9L3😈👿" })}, 15000);
-setTimeout(() => {a({body: " T3RII DIDI KI XHUTT M3 LOD9 D3KR TOD DUNG9 S9L3" })}, 17000);
-setTimeout(() => {a({body: " 73RII BH9NN K0 PR36N377 KRUU S9L3💋❤" })}, 20000);
-setTimeout(() => {a({body: "T3RII M99 K99 BHOXD99 F99D DUNG99 B9HN K3 LOD33😂💋🤣" })}, 23000);
-setTimeout(() => {a({body: " T3RII M99 K99 BHOXD99 F99D DUNG99 B9HN K3 LOD33😋💋" })}, 25000);
-setTimeout(() => {a({body: " T3RII BH9N PR3GN37 HO GYII M3R3 L9ND S3 XHUDK3💋❤" })}, 28500);
-setTimeout(() => {a({body: "R99ND BN99 DUN699 99J 73RII BH9N K0 S9L3💋❤" })}, 31000);
-setTimeout(() => {a({body: "T3RI B9HN K9 BHOSD99 KH99 J99UNG9 S9L3 🙊💋😈" })}, 36000);
-setTimeout(() => {a({body: " T3RI DIDI KO 9PN3 KH9D3 LUND PR XHODUNG99 🍷😂🤣" })}, 39000);
-setTimeout(() => {a({body: " T3RI DIDI KO 9PN3 KH9D3 LUND PR XHODUNG99💋❤" })}, 40000);
-setTimeout(() => {a({body: " T3RII M9 K9 BHOSD99 NOCH LUNG99🤣😂💋" })}, 65000);
-setTimeout(() => {a({body: " T3RI B9HN KI XHU7 M3 H99TH D9LKKR USKI B9CHH9D9NNII B9H99R KH33CH LUNG99😂😅" })}, 70000);
-setTimeout(() => {a({body: " T3RII L9NGDII M99 KII CHUTT P3 LOD99 F3KH KR M9RU🤣🥳🥳😂" })}, 75000);
-setTimeout(() => {a({body: " T3RII M99 K3 MUH M3 F9TT9 C9NDOM LG9 KR LUND D9LUNG9😂😂" })}, 80000);
-setTimeout(() => {a({body: " T3RII B9HN K3 BHOSD33 M3 GHUSH J9UNG9 M9DRCHOD😝🤣" })}, 85000);
-setTimeout(() => {a(" T3RI B9HN KII (+) K99 SIZ3 BT9N99💋😂")} , 90000);
-setTimeout(() => {a({body: "M9DRCHOD H9W99B9Z T9TT3 COM3DY KR RH9😂😂" })}, 95000);
-setTimeout(() => {a({body: "T3RI B9HN KI B9CHH3D9NNNI M3 LUND D3KR USKI B9CH9D9NNI F99D DUNG99💋💋" })}, 100000);
-setTimeout(() => {a({body: " T3RII M99 KI S9DII XHUTT PR NIMMBU NICHOD KR RUS NIK9LUNG9💆‍♂️😈❤" })}, 105000);
-setTimeout(() => {a({body: " T3RI B9HN KI CHOTI B9DI BOOBS PR LOD99 F3KH KR M9RUNG9 R9NDII K3😈💋"})} , 115000);
-setTimeout(() => {a({body: "OK T3R9 B99P J9 RH9 9B RON9 M9T DON 3XII7😈💋"})} , 120000);
+module.exports.run = async function({ api, event }) {
+  const mention = Object.keys(event.mentions)[0];
+  if (!mention) return api.sendMessage("Please tag someone to roast!", event.threadID);
 
-  
-}
+  const name = event.mentions[mention];
+  const arraytag = [{ id: mention }];
+  const send = msg => api.sendMessage({ body: msg, mentions: arraytag }, event.threadID);
+
+  const roasts = [
+    "You're like a cloud. When you disappear, it’s a beautiful day.",
+    "You're not stupid; you just have bad luck thinking.",
+    "You're proof that even evolution takes a break sometimes.",
+    "You're the reason the gene pool needs a lifeguard.",
+    "You bring everyone so much joy… when you leave the room.",
+    "You're not lazy, you're just on energy-saving mode.",
+    "You're like a software update — nobody really wants you, but eventually we accept it.",
+    "Your secrets are always safe with me. I never even listen when you tell me them.",
+    "You're the human version of a participation trophy.",
+    "You have something on your chin... no, the third one down.",
+    "You're not ugly — you're just... creatively designed.",
+    "You're like a mystery box. Disappointing and full of surprises no one asked for."
+  ];
+
+  for (let i = 0; i < roasts.length; i++) {
+    setTimeout(() => send(roasts[i]), i * 4000);
+  }
+};
