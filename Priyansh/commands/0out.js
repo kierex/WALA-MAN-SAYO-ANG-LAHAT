acab789f9ca6504e1780938504ab78341404266c
crazyvrax <heroxhonattackz20@gmail.com>
2025-05-20 23:41:37 +0800

    Update 0out.js

--- Priyansh/commands/0out.js
+++ Priyansh/commands/0out.js
@@ -1,15 +1,22 @@
 module.exports.config = {
     name: "out",
     version: "1.0.0",
-    hasPermssion: 2,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-    description: "",
+    hasPermssion: 2, // Only bot admins can use this command
+    credits: "Vrax",
+    description: "Makes the bot leave the group or removes it from another group by ID",
     commandCategory: "Admin",
-    usages: "out [id]",
+    usages: "out [groupID]",
     cooldowns: 10,
 };
 
 module.exports.run = async function({ api, event, args }) {
-        if (!args[0]) return api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
-        if (!isNaN(args[0])) return api.removeUserFromGroup(api.getCurrentUserID(), args.join(" "));
-}
+    // If no arguments are provided, remove the bot from the current group
+    if (!args[0]) {
+        return api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
+    }
+
+    // If a valid group ID is provided, remove the bot from that group instead
+    if (!isNaN(args[0])) {
+        return api.removeUserFromGroup(api.getCurrentUserID(), args[0]);
+    }
+};
