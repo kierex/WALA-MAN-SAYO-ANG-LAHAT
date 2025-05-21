e640e6635fd1d7bb60e52a82e3b8e3311ee43e61
crazyvrax <heroxhonattackz20@gmail.com>
2025-05-21 08:54:42 +0800

    Update fbcover.js

--- Priyansh/commands/fbcover.js
+++ Priyansh/commands/fbcover.js
@@ -2,182 +2,166 @@ module.exports.config = {
   name: "fbcover",
   version: "1.0.9",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "vern",
   description: "generate a custom facebook cover.",
   commandCategory: "generate-image",
   cooldowns: 0,
   usage: "<blank>",
   dependencies: {
-      "fs-extra": "",
-      "request": "",
-      "axios": ""
+    "fs-extra": "",
+    "request": "",
+    "axios": ""
   }
 };
 
-module.exports.run = async function ({ api, args, event, permssion , handleReply}) {
-const request = require('request');
-const fs = require("fs-extra")
-const axios = require("axios")
-const { threadID, messageID, senderID, body } = event;
-  if (this.config.credits != '\u0063\u0068\u0069\u006e\u0068\u006c\u0065 \u0026 \u0052\u0065\u002d\u004d\u0061\u0064\u0065 \u0062\u0079 \u0053\u0061\u0069\u006b\u0069\u0044\u0065\u0073\u0075') {
-        console.log(`\x1b[33m[ \u0057\u0041\u0052\u004e ]\x1b[37m » \u0043\u0068\u0061\u006e\u0067\u0065 \u0063\u0072\u0065\u0064\u0069\u0074\u0073 \u006d\u006f \u0070\u0061 \u0068\u0061\u002c \u006d\u0061\u0074\u0075\u0074\u006f \u006b\u0061 \u006d\u0061\u0067\u0063\u006f\u0064\u0065 \u006e\u0067 \u0073\u0061\u0072\u0069\u006c\u0069 \u006d\u006f \u0075\u006c\u006f\u006c \u0070\u0061\u006b\u0079\u0075\u0021`);
-        return api.sendMessage('\u005b \u0057\u0041\u0052\u004e \u005d \u0044\u0065\u0074\u0065\u0063\u0074 \u0062\u006f\u0074 \u006f\u0070\u0065\u0072\u0061\u0074\u006f\u0072 ' + global.config.BOTNAME + ' \u0063\u0068\u0061\u006e\u0067\u0065 \u0063\u0072\u0065\u0064\u0069\u0074\u0073 \u006d\u006f\u0064\u0075\u006c\u0065\u0073  "' + this.config.name + '"', threadID, messageID);
+module.exports.run = async function ({ api, args, event, permission, handleReply }) {
+  const request = require('request');
+  const fs = require("fs-extra");
+  const axios = require("axios");
+  const { threadID, messageID, senderID } = event;
+
+  if (this.config.credits !== 'vern') {
+    console.log(`[ WARN ] Credits have been changed from 'vern' — this is unauthorized.`);
+    return api.sendMessage(`[ WARN ] Unauthorized credit modification detected in "${this.config.name}"`, threadID, messageID);
   }
-  else if (!args[0]){
-    api.sendMessage(`\u0059\u006f\u0075 \u0077\u0061\u006e\u0074 \u0074\u006f \u0063\u006f\u006e\u0074\u0069\u006e\u0075\u0065\u003f \u0050\u006c\u0065\u0061\u0073\u0065 \u0072\u0065\u0070\u006c\u0079 \u0069\u0066 \u0079\u006f\u0075 \u0077\u0061\u006e\u0074 \u0061\u006e\u0064 \u0069\u0067\u006e\u006f\u0072\u0065 \u0074\u0068\u0069\u0073 \u0069\u0066 \u0079\u006f\u0075 \u0064\u006f\u006e\u0027\u0074\u002e`,event.threadID, (err, info) => {
-    
-     return global.client.handleReply.push({
+
+  if (!args[0]) {
+    return api.sendMessage(`You want to continue? Please reply if you want and ignore this if you don't.`, threadID, (err, info) => {
+      return global.client.handleReply.push({
         type: "characters",
         name: this.config.name,
         author: senderID,
         tenchinh: args.join(" ").toUpperCase(),
         messageID: info.messageID
       });
-  },event.messageID);
-}
-}
+    }, messageID);
+  }
+};
 
-module.exports.handleReply = async function({ api, event, args, handleReply, client, __GLOBAL, Threads, Users, Currencies }) {
-    const axios = require("axios");
-    const fs = require("fs-extra");
-    const request = require("request");
-    var info = await api.getUserInfo(event.senderID);
-    var nameSender = info[event.senderID].name;
-    var arraytag = [];
-        arraytag.push({id: event.senderID, tag: nameSender});
-    if (handleReply.author != event.senderID) return;
-    const {
-        threadID,
-        messageID,
-        senderID
-    } = event;
+module.exports.handleReply = async function ({ api, event, handleReply }) {
+  const axios = require("axios");
+  const fs = require("fs-extra");
+  const request = require("request");
+  const info = await api.getUserInfo(event.senderID);
+  const nameSender = info[event.senderID].name;
+  const arraytag = [{ id: event.senderID, tag: nameSender }];
 
-    switch (handleReply.type) {
-             case "characters": { 
-        	api.unsendMessage(handleReply.messageID);
-        	return api.sendMessage(`Reply to this message enter your primary name`,threadID , function (err, info) { 
-        	  return global.client.handleReply.push({ 
-        	  	type: 'subname',
-        	  	name: 'fbcover',
-        	  	author: senderID,
-        	  	characters: event.body,
-        	  	messageID: info.messageID
-        	  })
-        	}, messageID);
-        } 
-        
-  
-        case "subname": { 
-        	api.unsendMessage(handleReply.messageID);
-        	return api.sendMessage(`You choose ${event.body} as your main name\n(Reply to this message enter your secondary name)`,threadID , function (err, info) { 
-        		return global.client.handleReply.push({ 
-        			type: 'number',
-        			name: 'fbcover',
-        			author: senderID,
-                    characters: handleReply.characters,
-        			name_s: event.body,
-        			messageID: info.messageID
-        		})
-        	}, messageID);
-        }
+  if (handleReply.author !== event.senderID) return;
 
-        case "number": { 
-        	api.unsendMessage(handleReply.messageID);
-        	return api.sendMessage(`You have selected "${event.body}" as your secondary name\n(Reply to this message with your phone number)`,threadID , function (err, info) { 
-        	  return global.client.handleReply.push({ 
-        	  	type: 'address',
-        	  	name: 'fbcover',
-        	  	author: senderID,
-        	  	        	  	 
-             characters: handleReply.characters,
-           subname: event.body,
-              
-        			name_s: handleReply.name_s,
-        			
-        	  	messageID: info.messageID
-        	  })
-        	}, messageID);
-        }
+  const { threadID, messageID, senderID } = event;
 
-        case "address": { 
+  switch (handleReply.type) {
+    case "characters":
+      api.unsendMessage(handleReply.messageID);
+      return api.sendMessage(`Reply to this message enter your primary name`, threadID, (err, info) => {
+        global.client.handleReply.push({
+          type: 'subname',
+          name: 'fbcover',
+          author: senderID,
+          characters: event.body,
+          messageID: info.messageID
+        });
+      }, messageID);
 
+    case "subname":
+      api.unsendMessage(handleReply.messageID);
+      return api.sendMessage(`You chose ${event.body} as your main name\n(Reply to this message to enter your secondary name)`, threadID, (err, info) => {
+        global.client.handleReply.push({
+          type: 'number',
+          name: 'fbcover',
+          author: senderID,
+          characters: handleReply.characters,
+          name_s: event.body,
+          messageID: info.messageID
+        });
+      }, messageID);
 
-api.unsendMessage(handleReply.messageID);
-        	return api.sendMessage(`You have selected "${event.body}" as your phone number\n(Reply to this message with your adrress)`,threadID , function (err, info) { 
-        	  return global.client.handleReply.push({ 
-        	  	type: 'email',
-        	  	name: 'fbcover',
-        	  	author: senderID,
-                      	  	 
-            characters: handleReply.characters,
-           subname: handleReply.subname,
-              
-              number: event.body,
-              
-        			name_s: handleReply.name_s,
-              
-        	  	messageID: info.messageID
-        	  })
-        	}, messageID);
-    }
+    case "number":
+      api.unsendMessage(handleReply.messageID);
+      return api.sendMessage(`You have selected "${event.body}" as your secondary name\n(Reply to this message with your phone number)`, threadID, (err, info) => {
+        global.client.handleReply.push({
+          type: 'address',
+          name: 'fbcover',
+          author: senderID,
+          characters: handleReply.characters,
+          subname: event.body,
+          name_s: handleReply.name_s,
+          messageID: info.messageID
+        });
+      }, messageID);
 
+    case "address":
+      api.unsendMessage(handleReply.messageID);
+      return api.sendMessage(`You have selected "${event.body}" as your phone number\n(Reply to this message with your address)`, threadID, (err, info) => {
+        global.client.handleReply.push({
+          type: 'email',
+          name: 'fbcover',
+          author: senderID,
+          characters: handleReply.characters,
+          subname: handleReply.subname,
+          number: event.body,
+          name_s: handleReply.name_s,
+          messageID: info.messageID
+        });
+      }, messageID);
 
-        case "email": { 
-        	api.unsendMessage(handleReply.messageID);
-        	return api.sendMessage(`You have selected character "${event.body}" as an address. \n(Reply to this message your email address)`,threadID , function (err, info) { 
-        	  return global.client.handleReply.push({ 
-        	  	type: 'color',
-        	  	name: 'fbcover',
-        	  	author: senderID,
-        	  	characters: handleReply.characters,
-           subname: handleReply.subname,
-              
-              number: handleReply.number,
-              address: event.body,
-        			name_s: handleReply.name_s,
-        	  	messageID: info.messageID
-        	  })
-        	}, messageID);
-        }
-        
-        case "color": { 
-        	api.unsendMessage(handleReply.messageID);
-        	return api.sendMessage(`You have chosen "${event.body}" as your email address.\nEnter\nEnter your background color (note: enter the English name of the color - If you don't want to enter the color then enter "no")\n(Reply this message)`,threadID , function (err, info) {
-        		return global.client.handleReply.push({ 
-        			type: 'create',
-        			name: 'fbcover',
-        			author: senderID,
-        			characters: handleReply.characters,
-           subname: handleReply.subname,
-              
-              number: handleReply.number,
-              address: handleReply.address,
-           email: event.body,
+    case "email":
+      api.unsendMessage(handleReply.messageID);
+      return api.sendMessage(`You have selected "${event.body}" as your address.\n(Reply to this message with your email address)`, threadID, (err, info) => {
+        global.client.handleReply.push({
+          type: 'color',
+          name: 'fbcover',
+          author: senderID,
+          characters: handleReply.characters,
+          subname: handleReply.subname,
+          number: handleReply.number,
+          address: event.body,
+          name_s: handleReply.name_s,
+          messageID: info.messageID
+        });
+      }, messageID);
 
-        			name_s: handleReply.name_s,
-        			messageID: info.messageID
-        		})
-        	}, messageID)
-        }
-        case "create": {
-            var char = handleReply.characters;
-            var name = handleReply.name_s;
-          var subname = handleReply.subname;
-          var number = handleReply.number;
-            
-          
-          var address = handleReply.address;
-          var email = handleReply.email;
-          var uid = event.senderID;
-          var color = event.body;
-            api.unsendMessage(handleReply.messageID);
-            api.sendMessage(`Initializing...`,threadID, (err, info) => {
-            setTimeout(() => {
-            	api.unsendMessage(info.messageID);
-            	var callback = () => api.sendMessage({body:`Sender Name: ${nameSender}\nName: ${name}\nSub Name: ${subname}\nID: ${uid}\nColor: ${color}\nAddress: ${address}\nEmail: ${email}\nNumber: ${number}`,mentions: arraytag,attachment: fs.createReadStream(__dirname + "/cache/fbcover.png")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/fbcover.png"),event.messageID);
-                return request(encodeURI(`https://api.phamvandien.xyz/fbcover/v1?name=${name}&uid=${uid}&address=${address}&email=${email}&subname=${subname}&sdt=${number}&color=${color}&apikey=KeyTest`)).pipe(fs.createWriteStream(__dirname + '/cache/fbcover.png')).on('close', () => callback());
-            }, 1000);
-          }, messageID);
-        }
-    }
-}
+    case "color":
+      api.unsendMessage(handleReply.messageID);
+      return api.sendMessage(`You have chosen "${event.body}" as your email address.\nEnter your background color (or type "no" to skip)\n(Reply to this message)`, threadID, (err, info) => {
+        global.client.handleReply.push({
+          type: 'create',
+          name: 'fbcover',
+          author: senderID,
+          characters: handleReply.characters,
+          subname: handleReply.subname,
+          number: handleReply.number,
+          address: handleReply.address,
+          email: event.body,
+          name_s: handleReply.name_s,
+          messageID: info.messageID
+        });
+      }, messageID);
+
+    case "create":
+      const {
+        characters, subname, number, address, email, name_s
+      } = handleReply;
+      const uid = senderID;
+      const color = event.body;
+
+      api.unsendMessage(handleReply.messageID);
+      api.sendMessage(`Initializing...`, threadID, (err, info) => {
+        setTimeout(() => {
+          api.unsendMessage(info.messageID);
+
+          const url = encodeURI(`https://api.phamvandien.xyz/fbcover/v1?name=${name_s}&uid=${uid}&address=${address}&email=${email}&subname=${subname}&sdt=${number}&color=${color}&apikey=KeyTest`);
+          const outputPath = __dirname + "/cache/fbcover.png";
+
+          request(url).pipe(fs.createWriteStream(outputPath)).on('close', () => {
+            api.sendMessage({
+              body: `Sender Name: ${nameSender}\nName: ${name_s}\nSub Name: ${subname}\nID: ${uid}\nColor: ${color}\nAddress: ${address}\nEmail: ${email}\nNumber: ${number}`,
+              mentions: arraytag,
+              attachment: fs.createReadStream(outputPath)
+            }, threadID, () => fs.unlinkSync(outputPath), messageID);
+          });
+        }, 1000);
+      });
+      break;
+  }
+};
